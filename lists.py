9574d4f0b2ce37f2460aa45648e29c5c1e47bdc5
gharidev <haridevgireesh+1@gmail.com>
2019-02-07 15:31:44 +0530

    Lists Update

--- lists.py
+++ lists.py
@@ -1,2 +1,8 @@
-subjects=["Physics","Chemistry",3117]
+subjects=["Physics","Chemistry","Maths","Computer","Social"]
+print(subjects)
+print(subjects[0])
+print(subjects[0:3])
+subjects[0]="English"
+print(subjects)
+del subjects[1]
 print(subjects)
